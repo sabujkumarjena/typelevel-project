efd48fa1fcf71f056e80a9ed9af067ee6fa6eb13
sabuj <sabuj.kumar.jena@gmail.com>
2023-09-02 03:45:42 +0530

    running core jobs

---
+++ src/main/scala/com/allevite/jobsboard/whiteboard/JobsWhiteboard.scala
@@ -0,0 +1,49 @@
+package com.allevite.jobsboard.whiteboard
+
+import cats.effect.*
+import doobie.*
+import doobie.hikari.HikariTransactor
+import doobie.implicits.*
+import doobie.util.*
+import com.allevite.jobsboard.domain.job.*
+import com.allevite.jobsboard.core.*
+
+import scala.io.StdIn
+
+object JobsWhiteboard extends IOApp.Simple {
+  val postgresResource: Resource[IO, HikariTransactor[IO]] = for {
+    ec <- ExecutionContexts.fixedThreadPool(32)
+    xa <- HikariTransactor.newHikariTransactor[IO](
+      "org.postgresql.Driver",
+      "jdbc:postgresql:board",
+      "docker",
+      "docker",
+      ec
+    )
+  } yield xa
+
+  val jobInfo = JobInfo.minimal(
+    company = "Allevite",
+    title = "Software Engineer",
+    description = "best job",
+    externalUrl = "allevite.com",
+    remote = false,
+    location = "Anywhere"
+  )
+  override def run: IO[Unit] = postgresResource.use { xa =>
+    for {
+      jobs      <- LiveJobs[IO](xa)
+      _         <- IO(println("Ready. Next...")) >> IO(StdIn.readLine())
+      id        <- jobs.create("sabuj@allevite,com", jobInfo)
+      _         <- IO(println("Next...")) >> IO(StdIn.readLine())
+      list      <- jobs.all()
+      _         <- IO(println(s"All jobs: $list. Next...")) >> IO(StdIn.readLine())
+      _         <- jobs.update(id, jobInfo.copy(title = "Software rockstar"))
+      newJob    <- jobs.find(id)
+      _         <- IO(println(s"New job: $newJob. Next...")) >> IO(StdIn.readLine())
+      _         <- jobs.delete(id)
+      listAfter <- jobs.all()
+      _ <- IO(println(s"Deleted job. List now: $listAfter. Next...")) >> IO(StdIn.readLine())
+    } yield ()
+  }
+}
