a06d1ce0564148e4a490a009b77a93579257c6a3
sabuj <sabuj.kumar.jena@gmail.com>
2023-09-05 18:51:54 +0530

    Auth Http validation

--- src/main/scala/com/allevite/jobsboard/http/routes/AuthRoutes.scala
+++ src/main/scala/com/allevite/jobsboard/http/routes/AuthRoutes.scala
@@ -21,48 +21,54 @@ class AuthRoutes[F[_]: Concurrent: Logger] private (auth: Auth[F]) extends HttpV
     SecuredRequestHandler(authenticator)
   // POST /auth/login {LoginInfo} => 200 OK with Authorization: Bearer {jwt}
   private val loginRout: HttpRoutes[F] = HttpRoutes.of[F] { case req @ POST -> Root / "login" =>
-    val maybeJwtToken = for {
-      loginInfo  <- req.as[LoginInfo]
-      maybeToken <- auth.login(loginInfo.email, loginInfo.password)
-      _          <- Logger[F].info(s"User logging in : ${loginInfo.email}")
-    } yield maybeToken
-    maybeJwtToken map {
-      case Some(jwtToken) =>
-        authenticator.embed(Response(Status.Ok), jwtToken) // Authorization: Bearer
-      case None => Response(Status.Unauthorized)
+    req.validate[LoginInfo] { loginInfo =>
+      val maybeJwtToken = for {
+
+        maybeToken <- auth.login(loginInfo.email, loginInfo.password)
+        _          <- Logger[F].info(s"User logging in : ${loginInfo.email}")
+      } yield maybeToken
+      maybeJwtToken map {
+        case Some(jwtToken) =>
+          authenticator.embed(Response(Status.Ok), jwtToken) // Authorization: Bearer
+        case None => Response(Status.Unauthorized)
+      }
     }
   }
 
   // POST /auth/users { NewUserInfo } => 201 Created or BadRequest
   private val createUserRoute: HttpRoutes[F] = HttpRoutes.of[F] {
     case req @ POST -> Root / "users" =>
-      for {
-        newUserInfo  <- req.as[NewUserInfo]
-        maybeNewUser <- auth.signUp(newUserInfo)
-        resp <- maybeNewUser match {
-          case Some(user) => Created(user.email)
-          case None       => BadRequest(s"User with email ${newUserInfo.email} already exists")
-        }
-      } yield resp
+      req.validate[NewUserInfo] { newUserInfo =>
+        for {
+
+          maybeNewUser <- auth.signUp(newUserInfo)
+          resp <- maybeNewUser match {
+            case Some(user) => Created(user.email)
+            case None       => BadRequest(s"User with email ${newUserInfo.email} already exists")
+          }
+        } yield resp
+      }
   }
   // PUT /auth/users/password {NewPasswordInfo}{ Authorization: Bearer {jwt}} => 200 OK
   private val changePasswordRoute: AuthRoute[F] = {
     case req @ PUT -> Root / "users" / "password" asAuthed user =>
-      for {
-        newPasswordInfo  <- req.request.as[NewPasswordInfo]
-        maybeUserOrError <- auth.changePassword(user.email, newPasswordInfo)
-        resp <- maybeUserOrError match {
-          case Right(Some(_)) => Ok()
-          case Right(None)    => NotFound(FailureResponse(s"User ${user.email} not found.."))
-          case Left(_)        => Forbidden()
-        }
-      } yield resp
+      req.request.validate[NewPasswordInfo] { newPasswordInfo =>
+        for {
+
+          maybeUserOrError <- auth.changePassword(user.email, newPasswordInfo)
+          resp <- maybeUserOrError match {
+            case Right(Some(_)) => Ok()
+            case Right(None)    => NotFound(FailureResponse(s"User ${user.email} not found.."))
+            case Left(_)        => Forbidden()
+          }
+        } yield resp
+      }
   }
   // POST /auth/logout {Authorization: Bearer {jwt} } => 200 OK
   private val logoutRoute: AuthRoute[F] = { case req @ POST -> Root / "logout" asAuthed _ =>
     val token = req.authenticator
     for {
-      _ <- authenticator.discard(token)
+      _    <- authenticator.discard(token)
       resp <- Ok()
     } yield resp
   }
--- src/main/scala/com/allevite/jobsboard/http/validation/validators.scala
+++ src/main/scala/com/allevite/jobsboard/http/validation/validators.scala
@@ -5,6 +5,8 @@ import cats.*
 import cats.data.*
 import cats.data.Validated.*
 import cats.implicits.*
+import com.allevite.jobsboard.domain.auth.*
+import com.allevite.jobsboard.domain.user.NewUserInfo
 
 import java.net.URL
 import scala.util.{Failure, Success, Try}
@@ -13,12 +15,15 @@ object validators {
   case class EmptyField(fieldName: String) extends ValidationFailure(s"'$fieldName' is empty")
   case class InvalidUrl(fieldName: String) extends ValidationFailure(s"'$fieldName' is invalid url")
   // empty field, invalid URL, invalid email
-
+  case class InvalidEmail(fieldName: String)
+      extends ValidationFailure(s"'$fieldName' is not a valid email")
   type ValidationResult[A] = ValidatedNel[ValidationFailure, A]
   trait Validator[A] {
     def validate(value: A): ValidationResult[A]
   }
-
+  // conditions
+  val emailRegex =
+    """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
   def validateRequired[A](field: A, fieldName: String)(
       required: A => Boolean
   ): ValidationResult[A] = if (required(field)) field.validNel
@@ -29,6 +34,11 @@ object validators {
       case Success(_) => field.validNel
       case Failure(_) => InvalidUrl(fieldName).invalidNel
     }
+
+  def validateEmail(field: String, fieldName: String): ValidationResult[String] =
+    if (emailRegex.findFirstMatchIn(field).isDefined) field.validNel
+    else InvalidEmail(fieldName).invalidNel
+
   given jobInfoValidator: Validator[JobInfo] = (jobInfo: JobInfo) => {
     val JobInfo(
       company,     // should not be empty
@@ -68,9 +78,44 @@ object validators {
       image.validNel,
       seniority.validNel,
       other.validNel
-    ).mapN(JobInfo.apply) //ValidatedNel[ValidationFailure, JobInfo]
-
-
+    ).mapN(JobInfo.apply) // ValidatedNel[ValidationFailure, JobInfo]
   }
+// create validators for
+//loginInfo
+  given loginInfoValidator: Validator[LoginInfo] = (loginInfo: LoginInfo) => {
+    val validUserEmail = validateRequired(loginInfo.email, "email")(_.nonEmpty)
+      .andThen(e => validateEmail(e, "email"))
+    val validUserPassword = validateRequired(loginInfo.password, "password")(_.nonEmpty)
+    (validUserEmail, validUserPassword).mapN(LoginInfo.apply) // Validated[...., LoginInfo]
+  }
+//newUserInfo
+
+  given newUserInfoValidator: Validator[NewUserInfo] = (newUserInfo: NewUserInfo) => {
+    val validUserEmail = validateRequired(newUserInfo.email, "email")(_.nonEmpty)
+      .andThen(e => validateEmail(e, "email"))
+    val validUserPassword = validateRequired(newUserInfo.password, "password")(_.nonEmpty)
+    // you can enhance password validation logic here
+    (
+      validUserEmail,
+      validUserPassword,
+      newUserInfo.firstName.validNel,
+      newUserInfo.lastName.validNel,
+      newUserInfo.company.validNel
+    ).mapN(NewUserInfo.apply)
+  }
+//newPasswordInfo
+  given newPasswordInfoValidator: Validator[NewPasswordInfo] = (newPasswordInfo: NewPasswordInfo) =>
+    {
+
+      val validOldPassword =
+        validateRequired(newPasswordInfo.oldPassword, "oldPassword")(_.nonEmpty)
+      val validNewPassword =
+        validateRequired(newPasswordInfo.newPassword, "newPassword")(_.nonEmpty)
+      // you can enhance password validation logic here
+      (
+        validOldPassword,
+        validNewPassword
+      ).mapN(NewPasswordInfo.apply)
+    }
 
 }
