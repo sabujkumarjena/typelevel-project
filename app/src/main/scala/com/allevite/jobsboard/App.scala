c092380a41282331d81a6c3ac9ed4c69029c85f1
sabuj <sabuj.kumar.jena@gmail.com>
2023-09-07 03:13:46 +0530

    Tyrian

--- app/src/main/scala/com/allevite/jobsboard/App.scala
+++ app/src/main/scala/com/allevite/jobsboard/App.scala
@@ -3,13 +3,24 @@ package com.allevite.jobsboard
 import cats.effect.*
 
 import scala.scalajs.js.annotation.*
-import org.scalajs.dom.document
+import org.scalajs.dom.{console, document}
 import tyrian.*
 import tyrian.Html.*
+import tyrian.cmds.Logger
+
 import scala.concurrent.duration.*
+
+object App {
+  sealed trait Msg
+  case class Increament(amount: Int) extends Msg
+
+  case class Model(count: Int)
+
+}
 @JSExportTopLevel("AlleViteApp")
-class App extends TyrianApp[Int, String] {
+class App extends TyrianApp[App.Msg, App.Model] {
   //                     message  model = "state"
+  import App.*
 
   /*
     We can send messages by
@@ -17,24 +28,26 @@ class App extends TyrianApp[Int, String] {
     - create a subscription
     - listening for an event
    */
-  override def init(flags: Map[String, String]): (String, Cmd[IO, Int]) =
-    ("", Cmd.None)
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    (Model(0), Cmd.None)
 
   // potentially endless stream of messages
-  override def subscriptions(model: String): Sub[IO, Int] =
+  override def subscriptions(model: Model): Sub[IO, Msg] =
     Sub.None
   // Sub.every[IO](1.second).map(_ => 1)
 
   // model can change by recieving message
   // model => message => (new Model, _____)
   // update triggered whenever we get a new message
-  override def update(model: String): Int => (String, Cmd[IO, Int]) =
-    message => (model + ", " + message, Cmd.None)
-
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case Increament(amount) =>
+    // console.log("Changing count by" + amount)
+    (model.copy(count = model.count + amount), Logger.consoleLog[IO]("Changing count by " + amount))
+  }
   // view triggered whenever model changes
-  override def view(model: String): Html[Int] =
+  override def view(model: Model): Html[Msg] =
     div(
-      button(onClick(1))("click me"),
+      button(onClick(Increament(1)))("Increase"),
+      button(onClick(Increament(-1)))("Decrease"),
       div(s"Tyrian running: $model")
     )
 }
