5d859a5d10c327fc6ddea3ad8f18ddcfffd07127
sabuj <sabuj.kumar.jena@gmail.com>
2023-09-07 08:20:06 +0530

    Tyrian whiteboard

---
+++ app/src/main/scala/com/allevite/jobsboard/whiteboard/WhiteboardApp.scala
@@ -0,0 +1,52 @@
+package com.allevite.jobsboard.whiteboard
+
+import cats.effect.*
+import org.scalajs.dom.{console, document}
+import tyrian.*
+import tyrian.Html.*
+import tyrian.cmds.Logger
+
+import scala.concurrent.duration.*
+import scala.scalajs.js.annotation.*
+
+object WhiteboardApp {
+  sealed trait Msg
+  case class Increament(amount: Int) extends Msg
+
+  case class Model(count: Int)
+
+}
+//@JSExportTopLevel("AlleViteApp")
+class WhiteboardApp extends TyrianApp[WhiteboardApp.Msg, WhiteboardApp.Model] {
+  //                     message  model = "state"
+  import WhiteboardApp.*
+
+  /*
+    We can send messages by
+    - trigger a command
+    - create a subscription
+    - listening for an event
+   */
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    (Model(0), Cmd.None)
+
+  // potentially endless stream of messages
+  override def subscriptions(model: Model): Sub[IO, Msg] =
+    Sub.None
+  // Sub.every[IO](1.second).map(_ => 1)
+
+  // model can change by recieving message
+  // model => message => (new Model, _____)
+  // update triggered whenever we get a new message
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case Increament(amount) =>
+    // console.log("Changing count by" + amount)
+    (model.copy(count = model.count + amount), Logger.consoleLog[IO]("Changing count by " + amount))
+  }
+  // view triggered whenever model changes
+  override def view(model: Model): Html[Msg] =
+    div(
+      button(onClick(Increament(1)))("Increase"),
+      button(onClick(Increament(-1)))("Decrease"),
+      div(s"Tyrian running: ${model.count}")
+    )
+}
